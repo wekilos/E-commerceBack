aaee1b5694b70c77b3aafb8e0989361cfaf40066
wekilos <wekilyazgulyyew2000@gmail.com>
2023-06-07 20:18:09 +0500

    access localhost and ip

--- servers.js
+++ servers.js
@@ -26,30 +26,12 @@ const limiter = require("express-rate-limit")({
 });
 app.use("/", limiter);
 
-// app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" }));
 // app.use(
 //   helmet({
 //     crossOriginResourcePolicy: false,
 //   })
 // );
-
-const allowedOrigins = [
-  "http://localhost:3333/",
-  "http://119.235.118.211:3333/",
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
 
 // app.use(upload.single("surat"));
 app.use((req, res, next) => {
